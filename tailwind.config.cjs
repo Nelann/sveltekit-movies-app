5e4520526a95326f7a21e11d8da2bf03fbcfc5e0
NelanJoe <nelanjorelat@gmail.com>
2023-10-04 18:23:38 +0700

    add: theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config}*/
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-
 	theme: {
-		extend: {}
+		extend: {
+			daisyui: {
+				themes: ['light', 'dark', 'cupcake']
+			}
+		}
 	},
-
 	plugins: [require('daisyui')]
 };
 
