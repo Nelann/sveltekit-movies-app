365edbf3dc97d6a6d08ca6aaf91b1b024279dfc7
NelanJoe <nelanjorelat@gmail.com>
2023-10-05 14:09:35 +0700

    feat: error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<section class="max-w-[900px] md:mx-auto mx-4 h-screen grid place-content-center space-y-4">
+	<h1 class="font-semibold text-red-500 text-4xl text-center">
+		{$page?.status}: {$page?.error?.message}
+	</h1>
+	<h1 class="italic text-center">Oops. Something when wrong page...</h1>
+	<div class="text-center">
+		<a class="btn btn-primary" href="/">Back to home</a>
+	</div>
+</section>
